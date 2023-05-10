1579007db37596d56351714e576cc62553c98bf7
Kisaragi Marine <kisaragi.effective@gmail.com>
2023-05-10 11:41:32 +0900

    feat: 整数1個を解釈し、それをexit codeとして返すアセンブリを出力

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+//! C23のサブセットを解釈し、x86_64、System V ABIのアセンブリを出力するコンパイラ。
+use std::fmt::Display;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[clap(long)]
+    source: String,
+}
+
+struct Assembler;
+
+impl Assembler {
+    fn new() -> Self {
+        Self
+    }
+
+    fn emit(&mut self, assembly_line: impl Display) {
+        println!("{assembly_line}");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let source = args.source;
+    let i = source.parse::<i32>().expect("与えられたソースは数字ではありません");
+    let mut assembler = Assembler::new();
+    assembler.emit(".intel_syntax noprefix");
+    assembler.emit(".globl main");
+    assembler.emit("main:");
+    assembler.emit(format!("  mov rax, {i}"));
+    assembler.emit("  ret");
 }
